716c4c578941f9b6a054465caf9d42fcc44f22f4
Alexandr <efremovdol@gmail.com>
2019-03-01 19:03:22 +0700

    Test branch commit.

--- sample/src/main/java/com/shashavs/sample/MainActivity.kt
+++ sample/src/main/java/com/shashavs/sample/MainActivity.kt
@@ -8,6 +8,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //Test branch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
